93a3f74f5da01c0b578a88bc1e429aafc517bd05
Iryna Vovk <irynavovk.s@gmail.com>
2019-05-16 12:50:45 +0300

    Add Loading Component

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Load } from "../Main/Style";
+
+class Loading extends Component {
+  render() {
+    return (
+      <>
+        <Load>
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+          <div />
+        </Load>
+      </>
+    );
+  }
+}
+
+export default Loading;
