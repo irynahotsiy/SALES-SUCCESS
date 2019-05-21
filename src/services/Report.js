c76031937aba1b96515254adea810ff34e379f38
Iryna Vovk <irynavovk.s@gmail.com>
2019-05-21 17:41:08 +0300

    using https://jsonp.afeld.me/? to fix http/https limitation  and fixing convert problem when calculate

--- src/services/Report.js
+++ src/services/Report.js
@@ -1,5 +1,5 @@
-/* Use https://cors.io/? to fix http/https limitation */ 
-const FIXER_ENDPOINT = "https://cors.io/?http://data.fixer.io/api/latest?access_key=5e2e34b6141b185a648f1be0c2a84530&symbols=UAH,PLN,USD";
+/* Use https://jsonp.afeld.me/? to fix http/https limitation */
+const FIXER_ENDPOINT = "https://jsonp.afeld.me/?url=http%3A%2F%2Fdata.fixer.io%2Fapi%2Flatest%3Faccess_key%3D5e2e34b6141b185a648f1be0c2a84530%26symbols%3DUAH%2CPLN%2CUSD";
 
 let cachedRates = null;
 
@@ -13,23 +13,28 @@ function calculateReport(rates, items, targetCurrency) {
   let total = 0;
   for (let item of items) {
     let price = parseFloat(item.price);
-    
+
     // fixer free plan doesn't allow changing base
     if (targetCurrency === "EUR") {
       if (item.currency !== rates.base) {
         price = price / rates.rates[item.currency];
       }
     } else {
-      price = (price / rates.rates[item.currency]) * rates.rates[targetCurrency];
+      if (item.currency === "EUR") {
+        price = price * rates.rates[targetCurrency];
+      } else if (item.currency !== targetCurrency) {
+        price = (price / rates.rates[item.currency]) * rates.rates[targetCurrency];
+      }
     }
     total += price;
   }
   return total;
 }
 
-export async function prepareReport(items, targetCurrency){
+
+export async function prepareReport(items, targetCurrency) {
   if (!cachedRates) {
-    cachedRates =  await getRates();
+    cachedRates = await getRates();
   }
   return calculateReport(cachedRates, items, targetCurrency);
 }
